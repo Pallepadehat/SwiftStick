3c3ad27d7f255ceca9f9340d7922f999c25c15ed
Pallepadehat <patrick.jakobsen@icloud.com>
2026-01-03 23:28:39 +0100

    Initial Commit

---
+++ SwiftStick/SwiftStickApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftStickApp.swift
+//  SwiftStick
+//
+//  Created by Patrick Jakobsen on 03/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct SwiftStickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
